ef77736c25cef038b5aef9b9e95e877b665ac3af
zhang <a925529@163.com>
2026-06-11 10:30:51 +0800

    Fix MPEG4ObjectID member name: aacLC → AAC_LC

--- ios/RTSPCamera/AudioEncoder.swift
+++ ios/RTSPCamera/AudioEncoder.swift
@@ -46,7 +46,7 @@ class AudioEncoder {
         dstFormat = AudioStreamBasicDescription(
             mSampleRate: sampleRate,
             mFormatID: kAudioFormatMPEG4AAC,
-            mFormatFlags: UInt32(MPEG4ObjectID.aacLC.rawValue),
+            mFormatFlags: UInt32(MPEG4ObjectID.AAC_LC.rawValue),
             mBytesPerPacket: 0,
             mFramesPerPacket: 1024,
             mBytesPerFrame: 0,
